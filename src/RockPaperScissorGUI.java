a5796d06dd4c7e822e2864774e0f703acc9e5091
wdopen-nk <nihatsadigzada06@gmail.com>
2025-08-18 19:41:09 +0400

    Complete the BackEnd & Fix the typos

--- src/RockPaperScissorGUI.java
+++ src/RockPaperScissorGUI.java
@@ -3,79 +3,103 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RockPaperScissorGUI extends JFrame {
+// frontend
+public class RockPaperScissorGUI extends JFrame implements ActionListener{
     // player buttons
     JButton rockButton, paperButton, scissorButton;
 
+    // will display the choice of the computer
     JLabel computerChoice;
 
-    public RockPaperScissorGUI() {
-        // invoke JFrame constructor and add title to GUI
-        super("Rock, Paper, and Scissor");
+    // will display the score of the computer and the player
+    JLabel computerScoreLabel, playerScoreLabel;
 
-        // set the size of GUI
+    // backend obj
+    RockPaperScissorBackEnd rockPaperScissor;
+
+    public RockPaperScissorGUI(){
+        // invoke JFrame constructor and add title to the GUI
+        super("Rock Paper Scissor");
+
+        // set the size of the GUI
         setSize(450, 574);
 
-        // set layout to null to disable the layout management
+        // set layout to null to disable layout management, so we can use absolute positioning for the elements
+        // (i.e. setting x,y coordinates and width/height values)
         setLayout(null);
+
+        // terminate the java virtual machine when closing the GUI
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // load GUI in the middle of the screen everytime we run the application
+        // load GUI in the center of the screen every time we run the application
         setLocationRelativeTo(null);
 
+        // initialize the backend obj
+        rockPaperScissor = new RockPaperScissorBackEnd();
+
         // add GUI components
-        addGUIComponents();
+        addGuiComponents();
     }
 
-    private void addGUIComponents() {
-        // create score table
-        JLabel scoreTable = new JLabel("Computer: 0");
+    private void addGuiComponents(){
+        // create computer score label
+        computerScoreLabel = new JLabel("Computer: 0");
 
-        // set x, y coordinates and width/height, font size & text placement
-        scoreTable.setBounds(0, 43, 450, 30);
-        scoreTable.setFont(new Font("Dialog", Font.BOLD, 26));
-        scoreTable.setHorizontalAlignment(SwingConstants.CENTER);
+        // set x,y coordinates and width/height values
+        computerScoreLabel.setBounds(0, 43, 450, 30);
+
+        // set the font to have a font family of dialog, font-weight of bold, and a font-size of 26
+        computerScoreLabel.setFont(new Font("Dialog", Font.BOLD, 26));
+
+        // place the text in the center
+        computerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         // add to GUI
-        add(scoreTable);
+        add(computerScoreLabel);
 
-        // create computer choice;
+        // create computer choice
         computerChoice = new JLabel("?");
         computerChoice.setBounds(175, 118, 98, 81);
         computerChoice.setFont(new Font("Dialog", Font.PLAIN, 18));
         computerChoice.setHorizontalAlignment(SwingConstants.CENTER);
 
-        // create a black border
+        // create a black border around
         computerChoice.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         add(computerChoice);
 
-        // create player score table
-        JLabel playerScoreTable = new JLabel("Player: 0");
-        playerScoreTable.setBounds(0, 317, 450, 30);
-        playerScoreTable.setFont(new Font("Dialog", Font.BOLD, 26));
-        playerScoreTable.setHorizontalAlignment(SwingConstants.CENTER);
-        add(playerScoreTable);
+        // create player score label
+        playerScoreLabel = new JLabel("Player: 0");
+        playerScoreLabel.setBounds(0, 317, 450, 30);
+        playerScoreLabel.setFont(new Font("Dialog", Font.BOLD, 26));
+        playerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(playerScoreLabel);
+
+        // player buttons
 
         // rock button
         rockButton = new JButton("Rock");
         rockButton.setBounds(40, 387, 105, 81);
         rockButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+        rockButton.addActionListener(this);
         add(rockButton);
 
         // paper button
         paperButton = new JButton("Paper");
         paperButton.setBounds(165, 387, 105, 81);
         paperButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+        paperButton.addActionListener(this);
         add(paperButton);
 
         // scissor button
-        scissorButton = new JButton("Scissor");
-        scissorButton.setBounds(285, 387, 105, 81);
+        scissorButton = new JButton("Scissors");
+        scissorButton.setBounds(290, 387, 105, 81);
         scissorButton.setFont(new Font("Dialog", Font.PLAIN, 18));
+        scissorButton.addActionListener(this);
         add(scissorButton);
     }
 
-    private void showDialog(String message) {
+    // displays a message dialog which will show the winner and a try again button to play again
+    private void showDialog(String message){
         JDialog resultDialog = new JDialog(this, "Result", true);
         resultDialog.setSize(227, 124);
         resultDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -99,10 +123,28 @@ public class RockPaperScissorGUI extends JFrame {
                 resultDialog.dispose();
             }
         });
-
         resultDialog.add(tryAgainButton, BorderLayout.SOUTH);
 
         resultDialog.setLocationRelativeTo(this);
         resultDialog.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // get player choice
+        String playerChoice = e.getActionCommand().toString();
+
+        // play rock paper scissor and store result into string var
+        String result = rockPaperScissor.playRockPaperScissor(playerChoice);
+
+        // load computer's choice
+        computerChoice.setText(rockPaperScissor.getComputerChoice());
+
+        // update score
+        computerScoreLabel.setText("Computer: " + rockPaperScissor.getComputerScore());
+        playerScoreLabel.setText("Player: " + rockPaperScissor.getPlayerScore());
+
+        // display result dialog
+        showDialog(result);
     }
 }
